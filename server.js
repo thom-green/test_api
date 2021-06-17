529221ce5987f3f08d0d9e6ae45103d14203f7b8
clericthom <51236439+clericthom@users.noreply.github.com>
2021-06-17 11:41:09 +0100

    Update max limit fro express

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const app = express();
 
 app.use(helmet());
 app.use(compression()); //Compress all routes
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+
 
 app.use('/', routes); //to use the routes
 app.route('/')
